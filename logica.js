cf56580641f905f4827cc9b8854e25a2177e45c6
Vinicius Maiores <vinimaiores@gmail.com>
2020-10-03 17:12:01 -0300

    primeiro commit

---
+++ logica.js
@@ -0,0 +1,12 @@
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+
+var n1 = parseFloat(numero1);
+var n2 = parseFloat(numero2);
+
+var result_soma = n1 + n2;
+
+var result_final = parseInt(result_soma);
+console.log(result_final);
+
+alert(result_soma);
